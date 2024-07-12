84d48cdf741a0e994d14d9133689a46103afbe99
JoãoDevel <joaovitormatosfontes15@gmail.com>
2024-07-12 03:49:31 +0100

    menu implementation 1.2

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const menu = document.querySelector(".show-menu")
 
 const openMenu = () => {
     menu.style.left = "0"
+    document.body.style.overflow = "hidden";
 }
 
 const closeMenu = () => {
     menu.style.left = "-110%"
+    document.body.style.overflow = "visible";
 }
 
 
